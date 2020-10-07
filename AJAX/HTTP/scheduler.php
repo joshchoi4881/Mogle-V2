474d00814bd609321800bb49cd3b6919666d470c
Josh Choi <joshchoi4881@gmail.com>
2020-10-07 19:20:07 -0400

    Testing

--- AJAX/HTTP/scheduler.php
+++ AJAX/HTTP/scheduler.php
@@ -10,15 +10,16 @@
     $start_date = $_POST["start_date"];
     $end_date = $_POST["end_date"];
     $data = array("location" => $location, "platform" => $platform, "start_date"=>$start_date, "end_date"=>$end_date);
-    $CURL = curl_init();
-    curl_setopt($CURL, CURLOPT_URL, $url);
-    curl_setopt($CURL, CURLOPT_POST, true);
-    curl_setopt($CURL, CURLOPT_POSTFIELDS, http_build_query($data));
-    curl_setopt($CURL, CURLOPT_CONNECTTIMEOUT, 0);
-    curl_setopt($CURL, CURLOPT_TIMEOUT, 0);
-    curl_setopt($CURL, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($CURL, CURLOPT_USERAGENT, "Mogle");
-    $result = curl_exec($CURL);
-    curl_close($CURL);
-    echo $result;
+    echo $data;
+    #    $CURL = curl_init();
+#    curl_setopt($CURL, CURLOPT_URL, $url);
+#    curl_setopt($CURL, CURLOPT_POST, true);
+#    curl_setopt($CURL, CURLOPT_POSTFIELDS, http_build_query($data));
+#    curl_setopt($CURL, CURLOPT_CONNECTTIMEOUT, 0);
+#    curl_setopt($CURL, CURLOPT_TIMEOUT, 0);
+#    curl_setopt($CURL, CURLOPT_RETURNTRANSFER, true);
+#    curl_setopt($CURL, CURLOPT_USERAGENT, "Mogle");
+#    $result = curl_exec($CURL);
+#    curl_close($CURL);
+#    echo $result;
 ?>
