6592c80e95a1f6a51d9b78da0a8a2433cc7c58ab
Josh Choi <joshchoi4881@gmail.com>
2020-10-07 22:35:46 -0400

    Testing

--- ML/server.py
+++ ML/server.py
@@ -179,7 +179,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config["CORS_HEADERS"] = "Content-Type"
 
 @app.route("/get_estimate", methods=["POST"])
